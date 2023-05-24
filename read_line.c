bc27539c418f3678ce58a5788765a420d259b8e5
Samuraxworld <Samuraxworld@gmail.com>
2023-05-24 12:01:46 +0100

    Simple_shell

---
+++ read_line.c
@@ -0,0 +1,16 @@
+#include "samuel.h"
+
+/**
+ * read_line - read a line.
+ *
+ * Return: NULL or pointer to string.
+ */
+
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+
+	getline(&line, &bufsize, stdin);
+	return (line);
+}
